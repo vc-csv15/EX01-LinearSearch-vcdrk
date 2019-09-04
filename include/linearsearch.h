66bae384c6f6f5b547908f1d08c1bdec21619306
aknight <aknight@vcccd.edu>
2019-09-04 14:44:03 -0700

    Turning it in

---
+++ include/linearsearch.h
@@ -0,0 +1,14 @@
+//
+// Created by aknight on 9/4/2019.
+//
+
+#ifndef EX01_LINEARSEARC_LINEARSEARCH_H
+#define EX01_LINEARSEARC_LINEARSEARCH_H
+namespace edu { namespace vcccd { namespace vc { namespace csv15 {
+template <class T>
+int64_t find(const T& value, T array[], size_t size)  {
+    // put your code here
+    return -1;
+}
+}}}}
+#endif //EX01_LINEARSEARC_LINEARSEARCH_H
---
+++ src/linearsearch.cc
@@ -0,0 +1,12 @@
+#include <cstdint>
+#include <cstdlib>
+
+namespace edu { namespace vcccd { namespace vc { namespace csv15 {
+
+                template <class T>
+                int64_t find(const T& value, T array[], size_t size) {
+                    // put your code here
+                    return -1;
+                }
+
+            }}}}
