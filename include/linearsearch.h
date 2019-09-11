239eb4684d9f91e3d90c0b54dc88a62ffa74bf3b
aknight <aknight@vcccd.edu>
2019-09-11 14:36:01 -0700

    I am not afraid of commitment. I should be committed

--- include/linearsearch.h
+++ include/linearsearch.h
@@ -8,6 +8,10 @@ namespace edu { namespace vcccd { namespace vc { namespace csv15 {
 template <class T>
 int64_t find(const T& value, T array[], size_t size)  {
     // put your code here
+    if (array == nullptr) return -1;
+    for (size_t i = 0; i < size; i++) {
+        if (value == array[i]) return i;
+    }
     return -1;
 }
 }}}}
--- src/linearsearch.cc
+++ src/linearsearch.cc
@@ -3,10 +3,10 @@
 
 namespace edu { namespace vcccd { namespace vc { namespace csv15 {
 
-                template <class T>
-                int64_t find(const T& value, T array[], size_t size) {
-                    // put your code here
-                    return -1;
-                }
+    template <class T>
+    int64_t find(const T& value, T array[], size_t size) {
+        // put your code here
+        return -1;
+    }
 
-            }}}}
+}}}}
